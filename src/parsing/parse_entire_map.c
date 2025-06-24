c6959bb5c5cd3174c2297884ac6cc1cf0acc96c6
jrandet <jrandet@student.42.fr>
2025-06-24 13:24:47 +0200

    check if directory is better, modular function instead of compact multi responsibility. Added error messages in parsing, DOES NOT MAKE

--- src/parsing/parse_entire_map.c
+++ src/parsing/parse_entire_map.c
@@ -6,32 +6,11 @@
 /*   By: jrandet <jrandet@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 15:31:51 by jrandet           #+#    #+#             */
-/*   Updated: 2025/06/24 13:11:56 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/06/24 13:24:42 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-/**
- * @param file_relative_path is the relative path to the file you are checking
- * the access to. 
- * GOAL: use the open function with O_DIRECTORY to check if the file is a dir.
- * if it opens the file and the fd is positive, then it succeeded and the file
- * is a directory. In this case we return true because it is dir.
- */
-bool	is_dir(char *file_relative_path)
-{
-	int	fd;
-
-	fd = open(file_relative_path, O_DIRECTORY);
-	if (fd > 0)
-	{
-		print_error_and_message("The named file is a directory.\n");
-		close(fd);
-		return (true);
-	}
-	return (false);
-}
 
 int	get_fd(t_main *main, char *file_relative_path)
 {
