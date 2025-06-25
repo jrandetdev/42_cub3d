680477022242738ccb28968b60ae18f7847ad21b
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-06-25 12:08:48 +0200

    add textures path in test_map.cub

---
+++ src/parsing/main_parsing.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_parsing.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 15:31:51 by jrandet           #+#    #+#             */
+/*   Updated: 2025/06/25 12:05:27 by hdougoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	get_fd(t_main *main, char *file_relative_path)
+{
+	int			fd;
+
+	fd = open(file_relative_path, O_RDONLY);
+	if (fd == -1)
+	{
+		if (errno == EACCES)
+			print_error_and_message("The required permissions are denied for the file.\n");
+		exit_cub3d(main, 1);
+	}
+	return (fd);
+}
+
+bool	parsing(t_main *main, char *file_relative_path)
+{
+	int		fd;
+	char	**file_content;
+
+	if (is_dir(file_relative_path))
+		exit_cub3d(main, 1);
+	fd = get_fd(main, file_relative_path);
+	file_content = get_file_content(main, fd, file_relative_path);
+	print_array(file_content);
+	//extract_map_elements(main, file_content);
+	get_map_descritpion(main, file_content);
+	free_string_array(&file_content);
+	is_map_valid(main);
+	return (true);
+}
