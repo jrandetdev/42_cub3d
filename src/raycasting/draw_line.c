bf7287b941ee9dee6a5a9d1b5140034dbeade29b
Jojorandet <josephine@randet.com>
2025-07-16 15:11:02 +0200

    rayuires bleues mais au moins pas de segfault et joli rendu

--- src/raycasting/draw_line.c
+++ src/raycasting/draw_line.c
@@ -1,14 +1,14 @@
 #include "cub3d.h"
 
-unsigned int	get_texture_pixel(t_main *main, int texture_x, int coor_y)
-{
-	long			pixel_offset;
-	unsigned int	pixel_color;
+// unsigned int	get_texture_pixel(t_main *main, int texture_x, int coor_y)
+// {
+// 	long			pixel_offset;
+// 	unsigned int	pixel_color;
 
-	pixel_offset = coor_y * main->wall.no.width + texture_x * main->wall.no.texture.bytespp;
-	pixel_color = *(int*)(main->wall.no.texture.addr + pixel_offset);
-	return (pixel_color);
-}
+// 	pixel_offset = coor_y * main->wall.no.width + texture_x * main->wall.no.texture.bytespp;
+// pixel_color = *(int*)(main->wall.no.texture.addr + pixel_offset);
+// 	return (pixel_color);
+// }
 
 static void	get_texture_position(t_main *main, t_texture texture, int line_height, int start, int end, int texture_x, int screen_x)
 {
@@ -16,15 +16,19 @@ static void	get_texture_position(t_main *main, t_texture texture, int line_heigh
 	double			step;
 	double			texture_pos;
 	int				y;
+	unsigned long	color;
 	
 	y = start;
 	step = 1.0 * texture.height / line_height;
+	//printf("step %f ", step);
 	texture_pos = (start - WIN_HEIGHT / 2 + line_height / 2) * step;
 	while (y < end)
 	{
 		tex_pos_y = (int)texture_pos & (texture.height - 1);
-		put_pixel_to_image(main, screen_x, y, get_texture_pixel(main, texture_x, tex_pos_y));
-		tex_pos_y += step;
+		//color = ((unsigned long *)main->wall.no.texture.addr)[main->wall.no.height * tex_pos_y + texture_x];
+		color = *(unsigned long *)main->wall.no.texture.addr + (tex_pos_y * main->wall.no.width + texture_x);
+		put_pixel_to_image(main, screen_x, y, color);
+		texture_pos += step;
 		y++;
 	}
 }
