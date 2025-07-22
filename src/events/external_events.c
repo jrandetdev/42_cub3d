e8b23203432097552effdf52406e52788f3010ec
Hayden Dougoud <hdougoud@c1r9s4.42lausanne.ch>
2025-07-22 15:56:41 +0200

    Norm OK with events folder

--- src/events/external_events.c
+++ src/events/external_events.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   external_events.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 15:52:16 by hdougoud          #+#    #+#             */
+/*   Updated: 2025/07/22 15:56:26 by hdougoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "cub3d.h"
 
@@ -13,9 +24,23 @@ int	mouse_move(int x, int y, t_main *main)
 			main->mouse.dist = (main->mouse.x - x) / 25;
 			main->mouse.dist = -main->mouse.dist;
 		}
-		mlx_mouse_move(main->mlx_ptr, main->mlx_win, WIN_WIDTH / 2, WIN_HEIGHT / 2);
+		mlx_mouse_move(main->mlx_ptr, main->mlx_win,
+			WIN_WIDTH / 2, WIN_HEIGHT / 2);
 	}
 	return (0);
+}
+
+static int	key_hit(int keycode, t_main *main)
+{
+	if (keycode == K_ESCP)
+		exit_cub3d(main, 0);
+	if (keycode == K_G)
+		main->keys.g = !main->keys.g;
+	if (keycode == K_M)
+		main->keys.m = !main->keys.m;
+	if (main->debug.last_keycode != keycode)
+		printf("keycode : %d\n", keycode);
+	main->debug.last_keycode = keycode;
 }
 
 static int	key_pressed(int keycode, t_main *main)
@@ -28,25 +53,17 @@ static int	key_pressed(int keycode, t_main *main)
 		main->keys.s = 1;
 	if (keycode == K_D)
 		main->keys.d = 1;
-	if (keycode == K_G)
-		main->keys.g = !main->keys.g;
-	if (keycode == K_M)
-		main->keys.m = !main->keys.m;
 	if (keycode == K_LEFT)
 		main->keys.left = 1;
 	if (keycode == K_RIGHT)
 		main->keys.right = 1;
-	if (keycode == K_ESCP)
-		exit_cub3d(main, 0);
 	if (keycode == K_ENTER)
 		main->keys.enter = 1;
 	if (keycode == K_UP)
 		main->keys.up = 1;
 	if (keycode == K_DOWN)
 		main->keys.down = 1;
-	if (main->debug.last_keycode != keycode) //debug
-		printf("keycode : %d\n", keycode); //debug
-	main->debug.last_keycode = keycode; //debug
+	key_hit(keycode, main);
 	return (1);
 }
 
@@ -73,11 +90,9 @@ static int	key_released(int keycode, t_main *main)
 	return (0);
 }
 
-
-
 void	init_external_events(t_main *main)
 {
 	mlx_hook(main->mlx_win, ON_KEYDOWN, 1L << 0, key_pressed, main);
 	mlx_hook(main->mlx_win, ON_KEYUP, 1L << 1, key_released, main);
-	mlx_hook(main->mlx_win, ON_MOUSEMOVE, 1L<<6, mouse_move, main);
+	mlx_hook(main->mlx_win, ON_MOUSEMOVE, 1L << 6, mouse_move, main);
 }
