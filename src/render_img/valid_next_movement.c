ac0f04a97f61f7976e3664a7e7b93178ea70a210
Jojorandet <josephine@randet.com>
2025-07-25 12:18:18 +0200

    valid movement modified in x before y for movement along the wall

--- src/render_img/valid_next_movement.c
+++ src/render_img/valid_next_movement.c
@@ -6,100 +6,104 @@
 /*   By: jrandet <jrandet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:03:07 by jrandet           #+#    #+#             */
-/*   Updated: 2025/07/23 15:10:18 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/07/25 12:15:43 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static bool	check_corner(t_main *main, double next_x, double next_y)
-{
-	char	**map;
+// static bool	check_corner(t_main *main, double next_x, double next_y)
+// {
+// 	char	**map;
 
-	map = main->map_struct.map;
-	if (map[(int)trunc(next_y - 0.1)][(int)trunc(next_x)] == '1'
-			&& map[(int)trunc(next_y)][(int)trunc(next_x + 0.1)] == '1')
-		return (false);
-	if (map[(int)trunc(next_y + 0.1)][(int)trunc(next_x)] == '1'
-			&& map[(int)trunc(next_y)][(int)trunc(next_x - 0.1)] == '1')
-		return (false);
-	if (map[(int)trunc(next_y + 0.1)][(int)trunc(next_x)] == '1'
-			&& map[(int)trunc(next_y)][(int)trunc(next_x + 0.1)] == '1')
-		return (false);
-	if (map[(int)trunc(next_y - 0.1)][(int)trunc(next_x)] == '1'
-			&& map[(int)trunc(next_y)][(int)trunc(next_x - 0.1)] == '1')
-		return (false);
-	return (true);
-}
+// 	map = main->map_struct.map;
+// 	if (map[(int)trunc(next_y - 0.1)][(int)trunc(next_x)] == '1'
+// 			&& map[(int)trunc(next_y)][(int)trunc(next_x + 0.1)] == '1')
+// 		return (false);
+// 	if (map[(int)trunc(next_y + 0.1)][(int)trunc(next_x)] == '1'
+// 			&& map[(int)trunc(next_y)][(int)trunc(next_x - 0.1)] == '1')
+// 		return (false);
+// 	if (map[(int)trunc(next_y + 0.1)][(int)trunc(next_x)] == '1'
+// 			&& map[(int)trunc(next_y)][(int)trunc(next_x + 0.1)] == '1')
+// 		return (false);
+// 	if (map[(int)trunc(next_y - 0.1)][(int)trunc(next_x)] == '1'
+// 			&& map[(int)trunc(next_y)][(int)trunc(next_x - 0.1)] == '1')
+// 		return (false);
+// 	return (true);
+// }
 
 void	valid_next_movement_up(t_main *main, double next_x, double next_y)
 {
+	double	current_x;
+	double	current_y;
 	double	valid_x;
 	double	valid_y;
 
-	valid_x = next_x + main->player.dir_x * 0.3;
-	valid_y = next_y + main->player.dir_y * 0.3;
-	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '0'
-		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '3')
-	{
-		if (check_corner(main, next_x, next_y))
-		{
-			main->player.x = next_x;
-			main->player.y = next_y;
-		}
-	}
+	current_x = main->player.x;
+	current_y = main->player.y;
+	valid_x = next_x + main->player.dir_x * 0.08;
+	valid_y = next_y + main->player.dir_y * 0.08;
+	if (main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '0'
+		|| main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '3')
+		main->player.x = next_x;
+	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '0'
+		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '3')
+		main->player.y = next_y;
 }
 
 void	valid_next_movement_down(t_main *main, double next_x, double next_y)
 {
+	double	current_x;
+	double	current_y;
 	double	valid_x;
 	double	valid_y;
 
-	valid_x = next_x - main->player.dir_x * 0.3;
-	valid_y = next_y - main->player.dir_y * 0.3;
-	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '0'
-		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '3')
-	{
-		if (check_corner(main, next_x, next_y))
-		{
-			main->player.x = next_x;
-			main->player.y = next_y;
-		} 
-	}
+	current_x = main->player.x;
+	current_y = main->player.y;
+	valid_x = next_x - main->player.dir_x * 0.08;
+	valid_y = next_y - main->player.dir_y * 0.08;
+	if (main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '0'
+		|| main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '3')
+		main->player.x = next_x;
+	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '0'
+		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '3')
+		main->player.y = next_y;
 }
 
 void	valid_next_movement_right(t_main *main, double next_x, double next_y)
 {
+	double	current_x;
+	double	current_y;
 	double	valid_x;
 	double	valid_y;
 
-	valid_x = next_x + main->player.plane_x * 0.3;
-	valid_y = next_y + main->player.plane_y * 0.3;
-	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '0'
-		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '3')
-	{
-		if (check_corner(main, next_x, next_y))
-		{
-			main->player.x = next_x;
-			main->player.y = next_y;
-		}
-	}
+	current_x = main->player.x;
+	current_y = main->player.y;
+	valid_x = next_x + main->player.plane_x * 0.08;
+	valid_y = next_y + main->player.plane_y * 0.08;
+	if (main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '0'
+		|| main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '3')
+		main->player.x = next_x;
+	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '0'
+		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '3')
+		main->player.y = next_y;
 }
 
 void	valid_next_movement_left(t_main *main, double next_x, double next_y)
 {
+	double	current_x;
+	double	current_y;
 	double	valid_x;
 	double	valid_y;
 
-	valid_x = next_x - main->player.plane_x * 0.3;
-	valid_y = next_y - main->player.plane_y * 0.3;
-	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '0'
-		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(valid_x)] == '3')
-	{
-		if (check_corner(main, next_x, next_y))
-		{
-			main->player.x = next_x;
-			main->player.y = next_y;
-		}
-	}
+	current_x = main->player.x;
+	current_y = main->player.y;
+	valid_x = next_x - main->player.plane_x * 0.08;
+	valid_y = next_y - main->player.plane_y * 0.08;
+	if (main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '0'
+		|| main->map_struct.map[(int)trunc(current_y)][(int)trunc(valid_x)] == '3')
+		main->player.x = next_x;
+	if (main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '0'
+		|| main->map_struct.map[(int)trunc(valid_y)][(int)trunc(current_x)] == '3')
+		main->player.y = next_y;
 }
